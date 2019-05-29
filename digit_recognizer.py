2580387d46129b1e5cfcf8c69d892ba014823941
Ricky J. Sparks <rickyjames1750@gmail.com>
2019-05-28 22:19:55 -0700

    Add: Added Handwritten Digit Recognition
    Handwritten Digit Recognition Using Scikit-Learn
    Resolves: No issues tracked 
    See also: No Issues tracked

---
+++ digit_recognizer.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn import metrics
+import joblib
+import numpy as np
+from sklearn.utils import shuffle
+
+
+##Step 1: Get Data from CSV
+dataframe = pd.read_csv('csv/dataset5labels.csv')
+dataframe = dataframe.sample(frac=1).reset_index(drop=True)
+
+print dataframe
+
+##Step 2: Seperate Labels and Features
+X = dataframe.drop(['label'], axis=1)
+Y = dataframe['label']
+#X_train, Y_train = X[0:198], Y[0:198]
+#X_test,Y_test = X[198:],Y[198:]
+X_train, Y_train = X, Y
+X_test,Y_test = X,Y
+
+##Step 3: Make sure you have the correct Feature / label combination in training
+grid_data = X_train.values[40].reshape(28,28)
+plt.imshow(grid_data,interpolation=None,cmap="gray")
+plt.title(Y_train.values[40])
+plt.show()
+
+
+##Step 4: Build a Model and Save it
+model = svm.SVC(kernel="linear",c=2)
+
+print "Fitting this might take some time......"
+
+model.fit(X_train,Y_train)
+
+joblib.dump(model, "model/svm_0to5label_linear_2")
+#model = joblib.load("svm_class_1")
+print "predicting ...."
+predictions = model.predict(X_test)
+
+##Step5 : Print Accuracy
+print "Getting Accuracy ......"
+print "Score", metrics.accuracy_score(Y_test, predictions)
